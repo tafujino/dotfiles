c257f344f64691274f83367ca3c2c7483ffe90eb
Takeshi Fujino <fujino@edu.k.u-tokyo.ac.jp>
2020-01-30 16:20:08 +0900

    FEAT: update ivy settings

--- .emacs.d/init/10-ivy.el
+++ .emacs.d/init/10-ivy.el
@@ -4,18 +4,23 @@
 (use-package ivy-hydra)
 
 (setq ivy-use-virtual-buffers t)
-(setq enable-recursive-minibuffers t)
-(setq ivy-extra-directories nil)
-(setq ivy-re-builders-alist
-      '((t . ivy--regex-plus)))
+;;(setq enable-recursive-minibuffers t)
+;;(setq ivy-extra-directories nil)
+;;(setq ivy-re-builders-alist
+;;      '((t . ivy--regex-plus)))
 (ivy-mode 1)
 
 ;; counsel
 (global-set-key (kbd "M-x") 'counsel-M-x)
-(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+;;(global-set-key (kbd "C-x C-f") 'counsel-find-file)
 (setq counsel-find-file-ignore-regexp (regexp-opt '("./" "../")))
 (global-set-key (kbd "C-c r") 'counsel-recentf)
+(require 'counsel-world-clock nil t)
 
 ;; swiper
 (setq swiper-include-line-number-in-search t)
 (global-set-key "\C-s" 'swiper)
+
+;; magit
+(with-eval-after-load "magit"
+  (setq magit-completing-read-function 'ivy-completing-read))
