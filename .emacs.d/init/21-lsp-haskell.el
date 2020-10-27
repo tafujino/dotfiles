746baadf4eef49ab44e2f9e3595088ecbafbded9
Takeshi Fujino <fujino@edu.k.u-tokyo.ac.jp>
2020-10-27 19:09:09 +0900

    FEAT: update hie to haskell-language-server

--- .emacs.d/init/21-lsp-haskell.el
+++ .emacs.d/init/21-lsp-haskell.el
@@ -3,13 +3,12 @@
 
 (use-package lsp-haskell)
 (require 'lsp-haskell)
-(setq lsp-haskell-process-path-hie "hie-wrapper")
+(setq lsp-haskell-process-path-hie "haskell-language-server-wrapper")
 
 (setq lsp-prefer-flymake nil)
 
 (lsp-register-client
- (make-lsp-client :new-connection (lsp-tramp-connection "hie-wrapper")
+ (make-lsp-client :new-connection (lsp-tramp-connection "haskell-language-server-wrapper")
                   :major-modes '(haskell-mode)
                   :remote? t
                   :server-id 'hie-remote))
-
