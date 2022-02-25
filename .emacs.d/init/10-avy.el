c6efed611772f67046bb3b5b5ee3cb44dd3a1555
Takeshi Fujino <fujino@edu.k.u-tokyo.ac.jp>
2022-02-25 11:39:34 +0900

    FEAT: use avy

--- .emacs.d/init/10-avy.el
+++ .emacs.d/init/10-avy.el
@@ -1,4 +1,3 @@
-;;(use-package avy)
-;;(global-set-key (kbd "C-;") 'avy-goto-char)
-;;(global-set-key (kbd "C-'") 'avy-goto-char-2)
-;;
+(use-package avy)
+(global-set-key (kbd "C-;") 'avy-goto-char)
+(global-set-key (kbd "C-'") 'avy-goto-char-2)
